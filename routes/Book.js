cb474363b8aeeae550682820c8d433ec6ca7fb64
B-rimel <luca.afev78@gmail.com>
2023-09-19 14:32:46 +0200

    Implémentation de la route auth

--- routes/Book.js
+++ routes/Book.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
-
+const auth = require("../middleware/auth");
+const multer = require("../middleware/multer");
 const bookCtrl = require("../controllers/Book");
 
 router.get("/", bookCtrl.getAllBooks);

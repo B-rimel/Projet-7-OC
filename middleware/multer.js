59d4b87f2a35ac51509b67b697844caf5dda92a4
B-rimel <luca.afev78@gmail.com>
2023-09-25 02:11:11 +0200

    Implémentation de Sharp

--- middleware/multer.js
+++ middleware/multer.js
@@ -1,29 +1,24 @@
 const multer = require("multer");
 const sharp = require("sharp");
+
 const MIME_TYPES = {
   "image/jpg": "jpg",
   "image/jpeg": "jpg",
   "image/png": "png",
 };
 
-const storage = multer.memoryStorage({
-  destination: (req, file, callback) => {
-    callback(null, "images");
-  },
-  filename: (req, file, callback) => {
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
+
+(module.exports = upload.single("image")),
+  async (req, res) => {
+    const { buffer, originalname } = req.file;
     const name = file.originalname.split(" ").join("_");
     const extension = MIME_TYPES[file.mimetype];
-    callback(null, name + Date.now() + "." + extension);
-  },
-});
-
-module.exports = multer({ storage: storage }).single("image");
-
-const { buffer, originalName } = req.file;
-const timeStamp = Date.now();
-const fileName = req.file.name;
-const imgUrl = fileName + timeStamp + req.file.extension;
-await sharp(buffer)
-  .webp({ quality: 50 })
-  .resize({ width: 400, height: 500 })
-  .toFile("./images/" + imgUrl);
+    const ref = name + Date.now() + "." + extension;
+    await sharp(buffer)
+      .webp({ quality: 50 })
+      .resize({ width: 500, height: 500 })
+      .toFile("./images/" + ref)
+      .then(console.log(ref));
+  };
